c108864dea22c3d94662cd715dd3bd36c3d322aa
Susanna McIntyre <susannamcintyre@SRM.local>
2022-09-07 15:55:10 +0200

    Implement project

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Suzy');
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor'; // import the script
+
+console.log(argv[2]);
+const color = randomColor({
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
+
+console.log(
+  chalk.hex(color)(
+    `################################
+################################
+################################
+####                        ####
+####        ${color}         ####
+####                        ####
+################################
+################################
+################################`,
+  ),
+);
